69e5b300ff1b9e0ccbe8bf66f7ea2468d9452070
sushilcs111 <sushil.sud@gmail.com>
2016-04-19 23:31:18 +0530

    update bhavana interface id in signup ctrl

--- js/controllers.js
+++ js/controllers.js
@@ -46,7 +46,7 @@ angular.module('your_app_name.controllers', ['ionic', 'ngCordova'])
 
 //LOGIN
         .controller('LoginCtrl', function ($scope, $state, $templateCache, $q, $rootScope, $ionicLoading, $timeout) {
-            window.localStorage.setItem('interface_id', '4');
+            window.localStorage.setItem('interface_id', '4'); 
             $scope.interface = window.localStorage.getItem('interface_id');
             $scope.doLogIn = function () {
                 $ionicLoading.show({template: 'Loading...'});
@@ -112,7 +112,7 @@ angular.module('your_app_name.controllers', ['ionic', 'ngCordova'])
             }, 30);
         })
         .controller('SignupCtrl', function ($scope, $state, $http, $rootScope) {
-            $scope.interface = window.localStorage.setItem('interface_id', '9');
+            $scope.interface = window.localStorage.setItem('interface_id', '4');
             $scope.user = {};
             $scope.user.name = '';
             $scope.user.email = '';
